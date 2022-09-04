2d9e03be61c14e265a191da94564b259e1b879b2
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-09-04 19:06:12 +0530

    orientation fixes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import {
   facingMapping,
-  furnishingMapping,
   houseTypeMapping,
   localityMapping,
   sampleInput,
   tenantsMapping,
-  typesMapping,
   waterSuppyMapping,
 } from "./utils/mappings";
 import Select from "react-select";
 import { predict } from "./api/requests";
+import {
+  ameneties,
+  fieldClassName,
+  filterTitle,
+  furnishing,
+  getGridClassName,
+  types,
+} from "./utils/utils";
 
 function App() {
   const [filters, setFilters] = useState({ ...sampleInput });
@@ -67,30 +73,20 @@ function App() {
             <h2 className="font-light text-gray-400 border-b border-gray-600 pb-1 text-2xl">
               Prediction
             </h2>
-            <div className="grid md:grid-cols-3 justify-items-center text-center py-8">
-              <div>
-                <div></div>
-                <div></div>
-              </div>
-              <div className="">
-                <div className="flex items-end justify-center space-x-2">
-                  {loading ? (
-                    <div className="text-7xl text-blue-600">Predicting...</div>
-                  ) : (
-                    <>
-                      <div className="text-2xl text-gray-400">Rs.</div>
-                      <div className="text-7xl text-green-500">{result}</div>
-                    </>
-                  )}
-                </div>
-                <div></div>
+            <div className="justify-items-center text-center py-8">
+              <div className="flex items-end justify-center space-x-2">
+                {loading ? (
+                  <div className="text-7xl text-blue-600">Predicting...</div>
+                ) : (
+                  <>
+                    <div className="text-2xl text-gray-400">Rs.</div>
+                    <div className="text-7xl text-green-500">{result}</div>
+                  </>
+                )}
               </div>
               <div></div>
             </div>
             <div className=" space-y-4">
-              <div className=" border-b border-gray-500 text-gray-400 font-light tracking-wider">
-                Other available prices
-              </div>
               <div className="space-x-4">
                 {[...Array(10).keys()].map((ele) => {
                   return (
@@ -466,53 +462,5 @@ function Filter({ onSubmit }) {
     </Formik>
   );
 }
-
-const types = [
-  { value: typesMapping["RK1"], label: "1 RK" },
-  { value: typesMapping["BHK1"], label: "1 BHK" },
-  { value: typesMapping["BHK2"], label: "2 BHK" },
-  { value: typesMapping["BHK3"], label: "3 BHK" },
-  { value: typesMapping["BHK4"], label: "4 BHK" },
-  { value: typesMapping["BHK4PLUS"], label: "4 BHK+" },
-];
-
-const furnishing = [
-  { value: furnishingMapping["FULLY_FURNISHED"], label: "Furnished" },
-  { value: furnishingMapping["SEMI_FURNISHED"], label: "Semi furnished" },
-  { value: furnishingMapping["NOT_FURNISHED"], label: "Not furnished" },
-];
-
-const filterTitle = "text-sm px-1 text-gray-300";
-
-const fieldClassName = (padding = false) =>
-  `block appearance-none w-full bg-transparent border font-light border-gray-600 hover:border-gray-500 px-2 py-2 pr-${
-    padding && 8
-  } rounded shadow leading-tight focus:outline-none focus:shadow-outline text-xs text-gray-400`;
-
-function getGridClassName(size = 2, gap = 1) {
-  return `grid grid-cols-${size} gap-${gap}`;
-}
-
-const ameneties = {
-  VP: 1,
-  PB: 1,
-  HK: 0,
-  STP: 1,
-  RWH: 1,
-  PARK: 0,
-  GP: 1,
-  SC: 1,
-  SECURITY: 1,
-  SERVANT: 0,
-  FS: 1,
-  CPA: 1,
-  GYM: 1,
-  CLUB: 1,
-  LIFT: 1,
-  INTERNET: 1,
-  AC: 0,
-  INTERCOM: 1,
-  POOL: 1,
-};
 
 export default App;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,53 @@
+import { furnishingMapping, typesMapping } from "./mappings";
+
 export function Capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const types = [
+  { value: typesMapping["RK1"], label: "1 RK" },
+  { value: typesMapping["BHK1"], label: "1 BHK" },
+  { value: typesMapping["BHK2"], label: "2 BHK" },
+  { value: typesMapping["BHK3"], label: "3 BHK" },
+  { value: typesMapping["BHK4"], label: "4 BHK" },
+  { value: typesMapping["BHK4PLUS"], label: "4 BHK+" },
+];
+
+export const furnishing = [
+  { value: furnishingMapping["FULLY_FURNISHED"], label: "Furnished" },
+  { value: furnishingMapping["SEMI_FURNISHED"], label: "Semi furnished" },
+  { value: furnishingMapping["NOT_FURNISHED"], label: "Not furnished" },
+];
+
+export const filterTitle = "text-sm px-1 text-gray-300";
+
+export const fieldClassName = (padding = false) =>
+  `block appearance-none w-full bg-transparent border font-light border-gray-600 hover:border-gray-500 px-2 py-2 pr-${
+    padding && 8
+  } rounded shadow leading-tight focus:outline-none focus:shadow-outline text-xs text-gray-400`;
+
+export function getGridClassName(size = 2, gap = 1) {
+  return `grid grid-cols-${size} gap-${gap}`;
+}
+
+export const ameneties = {
+  VP: 1,
+  PB: 1,
+  HK: 0,
+  STP: 1,
+  RWH: 1,
+  PARK: 0,
+  GP: 1,
+  SC: 1,
+  SECURITY: 1,
+  SERVANT: 0,
+  FS: 1,
+  CPA: 1,
+  GYM: 1,
+  CLUB: 1,
+  LIFT: 1,
+  INTERNET: 1,
+  AC: 0,
+  INTERCOM: 1,
+  POOL: 1,
+};
